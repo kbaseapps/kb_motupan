fe2e808e2d23d4c73d5caf31b8ef6fa46ccc410a
dcchivian <dcchivian@lbl.gov>
2023-07-18 00:25:50 +0000

    made base genome optional

--- lib/kb_motupan/kb_motupanImpl.py
+++ lib/kb_motupan/kb_motupanImpl.py
@@ -337,8 +337,10 @@ class kb_motupan:
             raise ValueError("unable to instantiate phylogenomics_Client. "+str(e))
 
         if not calling_params.get('pcp_input_genome_ref'):
+            self.log(console, "DETERMIONING CENTROID GENOME TO USE AS BASE")
             base_genome_ref = self.get_base_genome_ref (pangenome_upa, console)
         else:
+            self.log(console, "USING REQUESTED GENOME {} AS BASE".format(calling_params['pcp_input_genome_ref'])
             base_genome_ref = calling_params['pcp_input_genome_ref']
             
         circle_plot_params = {
